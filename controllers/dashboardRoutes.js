d1df8a6ddb398c407722d2817574022ef3590ad6
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-26 17:26:24 -0400

    adding dashboardroute

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,3 +2,29 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ],
+    })
+})
