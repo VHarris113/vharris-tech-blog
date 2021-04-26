85e33bd7fc6384159144e6246df5696e5443db9e
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-26 17:51:15 -0400

    finished dashboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -61,5 +61,24 @@ router.get('edit/:id', withAuth, (req, res) => {
             model: User,
             attributes: ['username']
         }
+    ]
     })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id.'});
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+    
+        res.render('edit-post', { post, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
+router.get('/new', (req, res) => {
+    res.render('new-post');
+});
+
+module.exports = router;
