ed847f79b3cdc6b5d71f1f1b866073138df512ec
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-26 16:00:09 -0400

    fixing the server and controller files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes.js');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
 
 module.exports = router;
