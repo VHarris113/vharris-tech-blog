83526e939ed068aec4f62c1b2b6a96edc8cab7ac
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-26 17:12:37 -0400

    finished controller homeroute js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -95,5 +95,27 @@ router.get('/posts-comments', (req, res) => {
             'title',
             'created_at'
         ],
-    })
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }    
+    ]
 })
+.then(dbPostData => {
+    if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id.'});
+        return;
+    }
+    const post = dbPostData.get({ plain: true });
+
+    res.render('posts-comments', { post, loggedIn: req.session.loggedIn });
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
