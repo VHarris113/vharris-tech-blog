45246f2da66181e534841c71446df5c666a9d95f
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-26 17:45:43 -0400

    working on the dashboard

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
         where: {
@@ -26,5 +27,39 @@ router.get('/', withAuth, (req, res) => {
             attributes: ['username']
         }
     ],
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
     })
 })
