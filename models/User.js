c8617fcf35c67354894a8a4257f495f511b26ca2
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-27 15:44:00 -0400

    finished model user

--- models/User.js
+++ models/User.js
@@ -16,23 +16,16 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [8]
       },
     },
   },
