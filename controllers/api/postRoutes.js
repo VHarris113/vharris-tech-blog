1d78dd292ce21990487c0ceea23d351919d815e5
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-26 20:13:54 -0400

    adding postroutes lines 90-99

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -34,4 +34,68 @@ router.get('/', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   });
+});
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'content',
+      'title',
+      'created_at'
+    ],
+    include: [{
+      model: User,
+      attributes: ['username']
+    },
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      include: {
+        model: User,
+        attributes: ['username']
+      }
+    }
+  ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'Post not found. Try again.' });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
+
+router.post('/', withAuth, (req, res) => {
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+    user_id: req.session.user_id
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+  Post.update({
+    title: req.body.title,
+    content: req.body.content
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }
+  )
 })
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const sequelize = require('../config/connection');
 const { Post, User, Comment} = require('../models');
 const router = require('express').Router();
+
 router.get('/', (req, res) => {
     Post.findAll({
         attributes: [
