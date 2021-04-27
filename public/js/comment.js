87b4f3ecf7f842e1943a461e5e425db32c762cf5
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-27 15:53:08 -0400

    adding commit.js

---
+++ public/js/comment.js
@@ -0,0 +1,7 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
+
+    const
+}
