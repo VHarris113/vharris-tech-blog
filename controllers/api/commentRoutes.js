14f9582300bf7db5b619353245a0aabae626ab65
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-26 20:54:38 -0400

    finished the comment routes coding

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,47 +1,65 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+const { route } = require('./userRoutes');
 
+router.get('/', (req, res) => {
+  Comment.findAll({})
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
 
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
+route.get('/:id', (req, res) => {
+  Comment.findOne({
+    where: {
+      id: req.params.id
     }
+  })
+  .then(dbCommentData => res.json(dbCommentData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
 
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
+router.post('/', withAuth, (req, res) => {
+  if(req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(404).json(err);
+    })
   }
 });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+router.put('/:id', withAuth, (req, res) => {
+  Comment.update({
+    comment_text: req.body.comment_text
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCommentData => {
+    if (!dbCommentData) {
+      res.status(404).json({ message: 'Comment not found. Try again.'});
+      return;
+    }
+    res.json(dbCommentData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
