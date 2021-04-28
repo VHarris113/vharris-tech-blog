4eba12816282eb1196b0c8cffe66bc110bd38c94
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-28 10:06:41 -0400

    fixing the jawsdb stuff

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.JAWSDB_DATABASE,
+    process.env.JAWSDB_USERNAME,
+    process.env.JAWSDB_PASSWORD,
     {
-      host: 'localhost',
+      host: process.env.JAWSDB_URL,
       dialect: 'mysql',
       port: 3306
     }
