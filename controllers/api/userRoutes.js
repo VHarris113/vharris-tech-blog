7084a5422f7ee12531da2556fbd931473a2d4be4
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-27 22:20:26 -0400

    found mistake in userRoutes, forgot to create

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -56,6 +56,26 @@ router.get('/', (req, res) => {
     })
 });
 
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 router.post('/login', (req, res) => {
     User.findOne({
         where: {
